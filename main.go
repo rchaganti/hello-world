94ea4c9d43e125253a5fffd5e18b84e23c627f43
Ravikanth C <ravikanth@ravichaganti.com>
2024-12-15 15:19:35 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/rchaganti/hello-world/cmd"
 )
 
 var (
-	version = "v0.0.7"
+	version = "v0.1.0"
 	commit  = "none"
 	date    = "unknown"
 )
 
 func main() {
 	// Set the version information
-	date = time.Now().Format(time.RFC3339)
+	//date = time.Now().Format(time.RFC3339)
 	cmd.SetVersionInfo(version, commit, date)
 
 	// call the root command's Execute function
